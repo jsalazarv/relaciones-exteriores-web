3e6dee5a2d693510d19f9b09d71c26c8f144c2a8
jsalazarv <jsalazarv8@gmail.com>
2024-02-19 15:38:40 -0600

    Implemented the use of composable to list employees

--- src/pages/Employees/EmployeesPage.vue
+++ src/pages/Employees/EmployeesPage.vue
@@ -1,7 +1,15 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <h1>Empleados</h1>
+    <ul>
+      <li v-for="employee in data?.data" :key="employee?.id">
+        {{ employee?.name }} {{ employee?.last_name }}
+      </li>
+    </ul>
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useEmployee } from 'src/composables/useEmployee';
+
+const { data } = useEmployee();
+</script>
